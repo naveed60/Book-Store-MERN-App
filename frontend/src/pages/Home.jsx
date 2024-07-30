b7ef1590bd21df12ff2798121228ddba3d2de184
Naveed Ali <naveed.ali@amroodlabs.com>
2024-07-30 13:49:44 +0500

    fix footer bug

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import { MdOutlineAddBox } from 'react-icons/md';
 import BooksTable from '../components/home/BooksTable';
 import BooksCard from '../components/home/BooksCard';
 import Footer from '../components/Footer';
@@ -29,31 +27,33 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='p-4'>
-      <div className='flex justify-center items-center gap-x-4'>
-        <select
-          className='bg-sky-100 hover:bg-sky-300 px-8 py-4 rounded-md'
-          value={showType}
-          onChange={(e) => setShowType(e.target.value)}
-        >
-          <option value='table'>Table View</option>
-          <option value='card'>Card View</option>
-        </select>
+    <div className='flex flex-col min-h-screen'>
+      <div className='flex-grow p-4'>
+        <div className='flex justify-center items-center gap-x-4'>
+          <select
+            className='bg-sky-100 hover:bg-sky-300 px-8 py-4 rounded-md'
+            value={showType}
+            onChange={(e) => setShowType(e.target.value)}
+          >
+            <option value='table'>Table View</option>
+            <option value='card'>Card View</option>
+          </select>
+        </div>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-3xl my-8'>Books List</h1>
+          <Link to='/books/create'>
+            <MdOutlineAddBox className='text-blue-700 text-6xl' />
+          </Link>
+        </div>
+        {loading ? (
+          <Spinner />
+        ) : showType === 'table' ? (
+          <BooksTable books={books} />
+        ) : (
+          <BooksCard books={books} />
+        )}
       </div>
-      <div className='flex justify-between items-center'>
-        <h1 className='text-3xl my-8'>Books List</h1>
-        <Link to='/books/create'>
-          <MdOutlineAddBox className='text-blue-700 text-6xl' />
-        </Link>
-      </div>
-      {loading ? (
-        <Spinner />
-      ) : showType === 'table' ? (
-        <BooksTable books={books} />
-      ) : (
-        <BooksCard books={books} />
-      )}
-      <Footer/>
+      <Footer />
     </div>
   );
 };
